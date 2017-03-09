cb4c912b9084550bfe147a3a1e34baf94a6b16bf
Nick Verlinden <nick@studiokitara.com>
2017-03-09 20:41:06 +0100

    Test with using.js as submodule.

--- bin/pkx-util.js
+++ bin/pkx-util.js
@@ -1,2 +1,14 @@
 #! /usr/bin/env node
-console.log("WIP");
+var program = require("commander");
+
+program
+    .arguments("<request> [file]")
+    //.option("-e, --embed", "If the destination file has an embed tag, the result of the request will be embedded.")
+    .option("-s, --save", "Creates a wrapped script that can be used for embedding.")
+    .action(function(request, file) {
+        //console.log("user: %s pass: %s file: %s", program.username, program.password, file);
+        if (program.save) {
+            console.log("Saving " + request);
+        }
+    })
+    .parse(process.argv);
