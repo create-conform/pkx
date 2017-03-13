fb704853f5f389faa6362e47b4a21cd40b2f8186
Nick Verlinden <nick@studiokitara.com>
2017-03-14 00:49:36 +0100

    Working!

--- bin/pkx-util.js
+++ bin/pkx-util.js
@@ -1,10 +1,17 @@
 #! /usr/bin/env node
 var program = require("commander");
+var using = require("../using.js/using.js");
+var error = require("../cc.error/error.js");
+var pkx = require("../cc.pkx/pkx.js");
 
 program
-    .arguments("<request> [file]")
+    .arguments("[file]")
     //.option("-e, --embed", "If the destination file has an embed tag, the result of the request will be embedded.")
-    .option("-s, --save", "Creates a wrapped script that can be used for embedding.")
+    .option("-b, --build [path]", "Creates a pkx from the given path.")
+    .option("-s, --save <request>", "Creates a wrapped script that can be used for embedding.")
+    .option("-t, --tree <request>", "Displays a dependency tree for the specified request.")
+    .option("-m, --minify", "Enables code minification.")
+    .option("--no-version", "Writes output files without version numbers in the file name.")
     .action(function(request, file) {
         //console.log("user: %s pass: %s file: %s", program.username, program.password, file);
         if (program.save) {
@@ -12,3 +19,10 @@ program
         }
     })
     .parse(process.argv);
+
+if (program.tree) {
+    console.log("Displaying module dependency tree." + pkx.PKX_SYSTEM);
+
+    //pkx.Require("./cc.error/").then(console.log, console.error);
+    using({ "package" : "https://raw.githubusercontent.com/create-conform/cc.dummy/master/", "raw" : false, "ignoreDependencies" : true }).then(console.log, console.error);
+}
